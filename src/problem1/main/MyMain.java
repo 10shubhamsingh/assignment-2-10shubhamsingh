e1968889dd1f4ab6c6a499fa30154f81248e3bd4
Shubham Singh <shubham.singh1_cs18@gla.ac.in>
2020-03-28 13:57:53 +0530

    Update MyMain.java
    Complete

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,19 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+
+public static void main(String[] args) {
+   MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+   binarySearchTree.insert(10);
+  binarySearchTree.insert(35);
+   binarySearchTree.insert(50);
+    binarySearchTree.insert(45);
+
+
+ System.out.println("Left child of the BinarySearch tree: ");
+ binarySearchTree.traverse();
+ System.out.println("Number of Parents not having their left child :");
+ System.out.println(binarySearchTree.getCountRight());
+
+}
 }
