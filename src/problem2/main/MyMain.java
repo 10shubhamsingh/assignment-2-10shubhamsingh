34a5c6bb2682c5ebae248d7bcd73c4b7b84c4daf
Shubham Singh <shubham.singh1_cs18@gla.ac.in>
2020-03-28 14:40:41 +0530

    Update MyMain.java
    Completed

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -4,15 +4,35 @@
  *  Date: 23-Mar-20
  *  Time: 7:15 PM
  */
+import problem1.mybst.MyBinarySearchTree;
+
+
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+  public static void main (String args[]){
+    MyBinarySearchTree obj=new MyBinarySearchTree();
+    
+    obj.insert(10);
+    obj.insert(8);
+      obj.insert(9);
+      obj.insert(6);
+      obj.insert(7);
+      obj.insert(2):
+    obj.insert(5);
+    obj.preorder(obj.root):
+    System.out.println();
+    obj.postorder(obj.root):
+    System.out.println("First statement is true");
+    System.out.println();
+    System.out.println("Second statement is false");
+      
   
   
   
   
-  
+ } 
   
   
   
