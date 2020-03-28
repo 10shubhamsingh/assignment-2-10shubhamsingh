d0088e54a2edb2960ea3586ff23d6a702e7cde5a
Shubham Singh <shubham.singh1_cs18@gla.ac.in>
2020-03-28 15:13:10 +0530

    Update MyMain.java
    Complete..

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,30 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+  
+  public static void main (String args[]){
+    
+    MyPriorityQueue queue = new MyPriorityQueue();
+    queue.enqueue(new Student ("Shubham",10));
+    queue.enqueue(new Student("Shikhar,49));
+     queue.enqueue(new Student("Kshitij",31)):
+     queue.enqueue(new Student("Abhishek",45));
+     queue.enqueue(new Student("Leo",36));
+     queue.enqueue(new Student("Arya",27));
+                              
+     queue.show();
+     queue.dequeue();
+     queue.show();
+             
+
+    
+    }
 }
