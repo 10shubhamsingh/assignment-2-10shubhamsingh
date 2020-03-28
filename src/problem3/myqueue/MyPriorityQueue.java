ca684f77b46927ed82aebc92667e3142ed2d865b
Shubham Singh <shubham.singh1_cs18@gla.ac.in>
2020-03-28 14:59:46 +0530

    Update MyPriorityQueue.java
    Finish

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,19 @@
  */
 package problem3.myqueue;
 
+  import problem3.myLinkedList.MyLinkedList;
+ import problem3.student.Student;
+
 public class MyPriorityQueue {
+  MyLinkedList linkedList = new MyLinkedList();
+  public void enqueue(Student student){
+    linkedList.addLast(student);
+    }
+  punlic void dequue(){
+    
+linkedList.deleteFirst();
 }
+  public void ahow(){
+   linkedList.diaplay();
+    }
+  }
